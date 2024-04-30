ed9fce8522eef430f0bdf1cdc399dfcd23eab51c
Sidney Medeiros <sidney.medeiros@dev.tribes.work>
2024-04-30 12:34:24 -0300

    Basic `Tenant Configuration`

---
+++ app/Http/Controllers/TenantController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreTenantRequest;
+use App\Http\Requests\UpdateTenantRequest;
+use App\Models\Tenant;
+
+class TenantController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreTenantRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Tenant $tenant)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateTenantRequest $request, Tenant $tenant)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Tenant $tenant)
+    {
+        //
+    }
+}
